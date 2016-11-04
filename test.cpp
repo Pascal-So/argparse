2f86f72ae788f224494aee3830d381d86102648f
Pascal Sommer <p@pascalsommer.ch>
2016-11-04 13:56:22 +0100

    Added a simple test case

--- test.cpp
+++ test.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 int main(int argc, char * argv[]){
   vector<option> v;
-  argparse(v, argc, argv);
+
+  v.push_back({0, "", "", false});
+  v.push_back({1, "i", "include", true});
+  v.push_back({2, "f", "", false});
+  v.push_back({3, "r", "", false});
+  v.push_back({4, "o", "outfile", true});
+  
+  auto result = argparse(v, argc, argv);
+
+  for(auto kv:result){
+      cout<<kv.first << " - " << kv.second<<"\n";
+  }
 }
